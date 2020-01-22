0f163c041f286096bcf5dda2a8ddf0bc7f76e885
Laura Lopez <laura.lopez1@etu.unice.fr>
2020-01-22 20:33:52 +0100

    init maven spring project

---
+++ src/test/java/fr/unice/polytech/si5/al/creditrama/teamd/coreapi/CoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.si5.al.creditrama.teamd.coreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
